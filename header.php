3385b7178cc3e797bb4040e95731c4dddac12847
guivini-ac <guiviniac@gmail.com>
2025-08-15 12:04:55 -0300

    Add charset and viewport meta tags

--- header.php
+++ header.php
@@ -7,7 +7,11 @@
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head><?php wp_head(); ?></head>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
